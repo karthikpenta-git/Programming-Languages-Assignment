f2f1ac004ab30f83ce12b4ca1deb1882936ebdb0
karthikpenta-git <kpenta48420@ucumberlands.edu>
2026-09-13 16:42:59 -0700

    Add Python type system example

---
+++ python_types.py
@@ -0,0 +1,15 @@
+value = 10
+
+print("Original value:", value)
+print("Original type:", type(value))
+
+value = "Ten"
+
+print("New value:", value)
+print("New type:", type(value))
+
+number1 = 5
+number2 = 3
+result = number1 + number2
+
+print("Calculation:", result)
