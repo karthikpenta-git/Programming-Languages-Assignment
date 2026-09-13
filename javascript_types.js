916067c6ee17214f4060aa0a33958177f2eab543
karthikpenta-git <kpenta48420@ucumberlands.edu>
2026-09-13 16:44:08 -0700

    Add JavaScript type system example

---
+++ javascript_types.js
@@ -0,0 +1,17 @@
+let value = 10;
+
+console.log("Original value:", value);
+console.log("Original type:", typeof value);
+
+value = "Ten";
+
+console.log("New value:", value);
+console.log("New type:", typeof value);
+
+let number1 = 5;
+let number2 = 3;
+let result = number1 + number2;
+
+console.log("Calculation:", result);
+
+console.log("Type coercion example:", "5" + 3);
