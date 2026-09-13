7fca2b5639e363b7a8ccda92b3815020b44f5c19
karthikpenta-git <kpenta48420@ucumberlands.edu>
2026-09-13 16:45:02 -0700

    Add C++ type system example

---
+++ cpp_types.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int value = 10;
+
+    cout << "Integer value: " << value << endl;
+
+    string text = "Ten";
+
+    cout << "String value: " << text << endl;
+
+    int number1 = 5;
+    int number2 = 3;
+    int result = number1 + number2;
+
+    cout << "Calculation: " << result << endl;
+
+    return 0;
+}
